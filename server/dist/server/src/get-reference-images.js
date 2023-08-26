9e8efe009048a26cf929581891b45a75c7b55319
Jared Hughes <jahughes241@gmail.com>
2023-08-26 14:02:05 -0700

    Remove dist files

--- server/dist/server/src/get-reference-images.js
+++
@@ -1,6 +0,0 @@
-import * as fs from "node:fs/promises";
-export function getReferenceImages() {
-    return Promise.all(new Array(8).fill(0).map((e, i) => {
-        return fs.readFile(`reference/${i}.png`);
-    }));
-}
