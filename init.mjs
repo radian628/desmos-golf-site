67d2d23e4f140f2a984b32e5f5071e56d811ba2b
radian628 <70352880+radian628@users.noreply.github.com>
2023-08-27 09:19:07 -0700

    added dev mode for build script

--- init.mjs
+++ init.mjs
@@ -1,6 +1,8 @@
 import * as process from "node:process";
 import { spawn } from "node:child_process";
 
+const isDev = process.argv[2] === "dev";
+
 function spawnWait(...args) {
   const proc = spawn(...args);
   return new Promise((resolve) => {
@@ -9,12 +11,33 @@ function spawnWait(...args) {
 }
 
 const npm = /^win/.test(process.platform) ? "npm.cmd" : "npm";
+const npx = /^win/.test(process.platform) ? "npx.cmd" : "npx";
 
-await spawnWait(npm, ["install"], { stdio: "inherit" });
-process.chdir("client/sandbox");
-await spawnWait(npm, ["install"], { stdio: "inherit" });
-await spawnWait(npm, ["run", "build"], { stdio: "inherit" });
+if (isDev) {
+  await spawnWait(npm, ["install"], { stdio: "inherit" });
+  process.chdir("client/sandbox");
+  await spawnWait(npm, ["install"], { stdio: "inherit" });
+  process.chdir("../../server");
+  await spawnWait(npm, ["install"], { stdio: "inherit" });
+  process.chdir("../");
 
-process.chdir("../../server");
-await spawnWait(npm, ["run", "build"], { stdio: "inherit" });
-await spawnWait(npm, ["start"], { stdio: "inherit" });
+  await spawnWait(
+    npx,
+    [
+      "concurrently",
+      "npm --prefix client/sandbox run dev",
+      "npm --prefix server run dev",
+      "npm --prefix server run start",
+    ],
+    { stdio: "inherit" }
+  );
+} else {
+  await spawnWait(npm, ["install"], { stdio: "inherit" });
+  process.chdir("client/sandbox");
+  await spawnWait(npm, ["install"], { stdio: "inherit" });
+  await spawnWait(npm, ["run", "build"], { stdio: "inherit" });
+
+  process.chdir("../../server");
+  await spawnWait(npm, ["run", "build"], { stdio: "inherit" });
+  await spawnWait(npm, ["start"], { stdio: "inherit" });
+}
